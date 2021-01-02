609567ce8220cc8b411959c14e2a89cf3ae38ec2
ConnorMul <mulhollandconnor@icloud.com>
2021-01-02 12:55:10 -0500

    commit tree 80873f4990ab06a0d3d0cab140d968895a0f03f3

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Your code here
+function createEmployeeRecord(empInfo) {
+    let empRecord = {
+        firstName: empInfo[0],
+        familyName: empInfo[1],
+        title: empInfo[2],
+        payPerHour: empInfo[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    
+    return empRecord
+}
+
+function createEmployeeRecords(empArrays) {
+    return empArrays.map((emp) => createEmployeeRecord(emp))
+}
+
+function createTimeInEvent(empRecord, dateStamp) {
+    let dateAndTime = dateStamp.split(" ")
+    
+    empRecord.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(dateAndTime[1], 10),
+        date: dateAndTime[0]
+    })
+    
+    
+    return empRecord
+}
+
+function createTimeOutEvent(empRecord, dateStamp) {
+    let dateAndTime = dateStamp.split(" ")
+    
+    empRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(dateAndTime[1], 10),
+        date: dateAndTime[0]
+    })
+
+    return empRecord
+}
+
+function hoursWorkedOnDate(empRecord, formDate) {
+    
+   let timeOut = empRecord.timeOutEvents.find(event => event.date === formDate)
+    
+   let timeIn = empRecord.timeInEvents.find(event => event.date === formDate)
+    
+   const hoursWorked = timeOut.hour - timeIn.hour
+    
+   return hoursWorked / 100
+}
+
+function wagesEarnedOnDate(empRecord, formDate) {
+    
+    let payOwed = hoursWorkedOnDate(empRecord, formDate) * empRecord.payPerHour
+    
+    return payOwed
+}
+
+function allWagesFor(empRecord) {
+    let datesWorked = empRecord.timeInEvents.map((event) => event.date)
+    
+    let payable = datesWorked.reduce((memo, date) => memo + wagesEarnedOnDate(empRecord, date), 0)
+
+    return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find((emp) => emp.firstName === firstName)
+}
+
+function calculatePayroll(empRecordsArray) {
+   return empRecordsArray.reduce((memo, record) => memo + allWagesFor(record), 0)
+}
